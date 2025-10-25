7c93f2df4ac5480a4cf196c4d9317f531e3d30c8
Napat Rattanaburi <napat254825@gmail.com>
2025-10-25 14:31:12 +0700

    I changed stuff

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -4,5 +4,8 @@ import { words } from "@/data/words";
 export async function GET() {
   const randomIndex = Math.floor(Math.random() * words.length);
   const word = words[randomIndex];
-  return NextResponse.json({ word });
+  // fetch localhost:8000
+
+  console.log("Cesar was here...")
+  return NextResponse.json({ data: word });
 }
