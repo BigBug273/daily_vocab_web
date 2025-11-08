553ae2daca17dbe23bf3729802f2a1dedb3bfd18
Napat Rattanaburi <napat254825@gmail.com>
2025-11-08 14:40:43 +0700

    Finished up the web

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -2,10 +2,11 @@ import { NextResponse } from "next/server";
 import { words } from "@/data/words";
 
 export async function GET() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const word = words[randomIndex];
+  // const randomIndex = Math.floor(Math.random() * words.length);
+  // const word = words[randomIndex];
   // fetch localhost:8000
-
+  const response = await fetch('https://localhost:8000/api/word');
+  const data = await response.json();
   console.log("Cesar was here...")
-  return NextResponse.json({ data: word });
+  return NextResponse.json({ data });
 }
